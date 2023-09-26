94bb9868b8a319fc72de1baa1a458f0e8db9d981
Utkarsh Pandey <utk2665@gmail.com>
2023-09-26 21:07:27 +0530

    Fixed Responsiveness to some extent

--- src/components/DisplayNotes.jsx
+++ src/components/DisplayNotes.jsx
@@ -5,11 +5,11 @@ import { REMOVE_TODO } from "./context/action.types";
 function DisplayNotes(){
     const {todos, dispatch} = useContext(contextTodo);
     return(
-        <div className="flex justify-center grid grid-cols-3 gap-1 m-10">
+        <div className="flex mx-auto justify-center grid xl:grid-cols-3 sm:grid-col-1 md:grid-cols-2 gap-1 p-4">
             {
                 todos.map((todo,index)=>{
                     return (
-                        <div key={index} className="rounded text-stone-50 font-mono bg-violet-500 border-violet-700 border-2 border-dotted p-[20px]">
+                        <div key={index} className="rounded m-1 text-stone-50 font-mono bg-violet-500 border-violet-700 border-2 border-dotted p-[20px]">
                             <h1>{todo}</h1>
                             <button className="bg-gray-200 p-1 m-1 font-mono text-black border rounded hover:bg-red-600 hover:text-white transition duration-450 ease-in-out" onClick={()=>{
                                 return dispatch({
